fdaac8ccd559f3d0f26680279a6cf5c46366466a
fjcamillo <fjcamillo.dev@gmail.com>
2019-01-06 03:03:05 +0800

    Add Postgres Database creation

--- backend/src/queries/create.sql
+++ backend/src/queries/create.sql
@@ -1,9 +1,10 @@
 create database house;
+\connect house;
 create table sensors (
     id BIGSERIAL PRIMARY KEY,
     created_at BIGINT,
     updated_at BIGINT,
-    deleted BIGINT,
+    deleted BOOLEAN,
     sensor_type TEXT,
     reading INTEGER
 );
