1338436a0251178ea374f98b83eb5c6de549303a
fjcamillo <fjcamillo.dev@gmail.com>
2019-01-06 13:38:07 +0800

    Update sensor routes and fix swagger for sensor routes"

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,15 +3,19 @@ import Router from 'koa-router'
 import bodyParser from 'koa-bodyparser'
 import * as swagger from 'swagger2'
 import {validate as swaggerValidate, ui as swaggerUI } from 'swagger2-koa'
+import { dbConnection } from './postgres'
+import cors from '@koa/cors'
 
-import { routes as pirRoutes } from './routes/pir'
+const origin = "*"
+
+import { routes as sensorRoutes } from './routes/sensors'
 
 
 const app = new Koa()
 const router  = new Router()
 
 for (const route of [
-    pirRoutes
+    sensorRoutes
 ]){
     route(router)
 }
@@ -26,7 +30,9 @@ router.get('/swagger.json', ctx => {
     ctx.status = 200
 })
 
+app.use(dbConnection())
 app.use(bodyParser())
+app.use(cors({origin}, spec))
 app.use(router.routes())
 app.use(swaggerUI(spec, '/explorer'))
 app.use(router.allowedMethods())
