0f328f24bbcf1d1dc4b4152d267252ac40ef58b4
fjcamillo.dev@gmail.com <fjcamillo.dev@gmail.com>
2019-04-27 23:41:12 +0800

    Fix backend config

--- backend/src/config.js
+++ backend/src/config.js
@@ -3,5 +3,6 @@ export const config = {
     DATABASE_PASSWORD: process.env["DATABASE_PASSWORD"],
     DATABASE_HOST: process.env["DATABASE_HOST"],
     DATABASE_NAME: process.env["DATABASE_NAME"],
-    DATABASE_PORT: process.env["DATABASE_PORT"]
+    DATABASE_PORT: process.env["DATABASE_PORT"],
+    BACKEND_PORT: process.env['BACKEND_PORT']
 }
